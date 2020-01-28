0f3e71ca08eed08e9959efc53a8cc2d941e1ae84
Razaul Munir <razaulmunir@gmail.com>
2020-01-28 22:07:36 +0600

    add intent

--- app/src/main/java/com/example/selfdevelopmentapplication/MainActivity.java
+++ app/src/main/java/com/example/selfdevelopmentapplication/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.selfdevelopmentapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    FloatingActionButton fb_addNew;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fb_addNew=findViewById(R.id.fb_addNew);
+
+        fb_addNew.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,AddTaskActivity.class));
+            }
+        });
     }
 }
