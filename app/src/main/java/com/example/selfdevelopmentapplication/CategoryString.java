c1cf58d4db2fc9d6771a22ebc99e61efd2d0529c
Razaul Munir <razaulmunir@gmail.com>
2020-01-28 22:22:54 +0600

    Array Strings added in CategoryString.java

---
+++ app/src/main/java/com/example/selfdevelopmentapplication/CategoryString.java
@@ -0,0 +1,52 @@
+package com.example.selfdevelopmentapplication;
+
+public class CategoryString {
+
+    public static String[] category = new String[]{
+            "Wrong",
+            "Motivation",
+            "Lesson",
+            "Story",
+            "Work",
+            "Technical",
+            "Habit",
+            "Behave",
+            "none"
+    };
+
+    public static String[] situation = new String[]{
+            "Decision",
+            "Thinking new",
+            "Awareness",
+            "Emergency",
+            "Relational",
+            "Health",
+            "Work and Career",
+            "Academic",
+            "Choice",
+            "Strategic",
+            "none"
+    };
+
+    public static String[] character = new String[]{
+            "Parents",
+            "Friend",
+            "Child",
+            "Senior",
+            "none"
+    };
+
+    public static String[] priority=new String[]{
+            "1",
+            "2",
+            "3",
+            "4",
+            "5"
+    };
+
+    public static String[] complete=new String[]{
+            "0",
+            "1"
+    };
+}
+
